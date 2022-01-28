89fb60972ff4119ea6b1de900acd2b7f682cce23
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-28 15:29:19 -0600

    Added more test cases

--- api/server.test.js
+++ api/server.test.js
@@ -33,7 +33,7 @@ describe("POST /api/auth/register", () => {
 });
 
 describe("POST /api/auth/login", () => {
-  describe("[3] user enters valid info", () => {
+  describe("user enters valid login info", () => {
     let res;
     beforeEach(async () => {
       res = await request(server)
@@ -41,15 +41,15 @@ describe("POST /api/auth/login", () => {
         .send({ username: "user", password: "password" });
     });
 
-    test("[3A] returns a status 200 OK", async () => {
+    test("[3] returns a status 200 OK", async () => {
       expect(res.status).toBe(200);
     });
-    test("[3B] returns the correct welcome message", async () => {
+    test("[4] returns the correct welcome message", async () => {
       expect(res.body.message).toBe("welcome, user");
     });
   });
-  describe("[4] user enters invalid info", () => {
-    test("returns a status 401", async () => {
+  describe("user enters invalid login info", () => {
+    test("[5] returns a status 401", async () => {
       let res = await request(server)
         .post("/api/auth/login")
         .send({ username: "wrong", password: "info" });
@@ -59,15 +59,29 @@ describe("POST /api/auth/login", () => {
 });
 
 describe("GET /api/jokes", () => {
-  let res;
-  beforeEach(async () => {
-    res = await request(server).get("/api/jokes");
+  describe("invalid request to pull joke data", () => {
+    let res;
+    beforeEach(async () => {
+      res = await request(server).get("/api/jokes");
+    });
+
+    test("[6] returns a status 401 if you aren't logged in", async () => {
+      expect(res.status).toBe(401);
+    });
+    test("[7] returns correct error message on rejection", async () => {
+      expect(res.body.message).toBe("token required");
+    });
   });
 
-  test("[5] returns a status 401 if you aren't logged in", async () => {
-    expect(res.status).toBe(401);
-  });
-  test("[6] returns correct error message on rejection", async () => {
-    expect(res.body.message).toBe("token required");
+  describe("user successfully pulls data", () => {
+    test("[8] returns jokes on authorized login", async () => {
+      let res = await request(server)
+        .post("/api/auth/login")
+        .send({ username: "user", password: "password" })
+      let res2 = await request(server)
+        .get("/api/jokes")
+        .set("Authorization", res.body.token);
+      expect(res2.status).toBe(200);
+    });
   });
 });
