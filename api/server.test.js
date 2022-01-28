bb3931e6a40adb1bbbf13ef50c1ba31419f56210
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-28 13:41:52 -0600

    Progress on server tests

--- api/server.test.js
+++ api/server.test.js
@@ -11,24 +11,47 @@ beforeAll(async () => {
   await db.migrate.rollback();
   await db.migrate.latest();
 });
-beforeEach(async () => {
-  await db.seed.run();
-});
+// beforeEach(async () => {
+//   await db.seed.run();
+// });
 afterAll(async () => {
   await db.destroy();
 });
 
 describe("POST /api/auth/register", () => {
-  test.todo('');
-  test.todo('');
+  let res;
+  beforeEach(async () => {
+    res = await request(server)
+      .post("/api/auth/register")
+      .send({ username: "user", password: "password" });
+  });
+
+  test("returns a status 201 and complete user object", () => {
+    expect(res.status).toBe(201);
+    expect(res.body).toMatchObject({ id: 1, username: "user" });
+  });
+  test("prevents duplicate usernames with proper messaging", () => {
+    expect(res.body.message).toBe("username taken");
+  });
 });
 
 describe("POST /api/auth/login", () => {
-  test.todo('');
-  test.todo('');
+  let res
+  beforeEach(async () => {
+    res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "user", password: "password" });
+  });
+
+  test("returns a status 200 OK", async () => {
+    expect(res.status).toBe(200);
+  });
+  test("returns the correct welcome message", async () => {
+    expect(res.body.message).toBe("welcome, user");
+  });
 });
 
 describe("GET /api/jokes", () => {
-  test.todo('');
-  test.todo('');
+  test("", () => {});
+  test.todo("");
 });
